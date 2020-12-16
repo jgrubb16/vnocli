79b975ce695534649be33acf90e78e43438704bc
Jordan Grubb <imjordangrubb@gmail.com>
2020-12-16 16:58:35 -0500

    removed cli

--- mod.ts
+++ mod.ts
@@ -39,12 +39,12 @@ const helloVno: string =
   <p>
     For a guide and preview of our osLabs repo<br>
     check out 
-    <a href="https://github.com/oslabs-beta/vno" target="_blank" rel="noopener">vue-cli documentation</a>.
+    <a href="https://github.com/oslabs-beta/vno" target="_blank" rel="noopener">  vue-cli documentation</a>.
   </p>
-  <h3>Installed CLI Plugins</h3>
-  <ul>
-    <li><a href="https://github.com/jgrubb16/vnocli" target="_blank" rel="noopener">babel</a></li>
-  </ul>
+  // <h3>Installed CLI Plugins</h3>
+  // <ul>
+  //   <li><a href="https://github.com/jgrubb16/vnocli" target="_blank" rel="noopener">babel</a></li>
+  // </ul>
 </div>
 </template>
 <script>
