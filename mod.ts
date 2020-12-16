1c6a2b5ace1dfe659c48957821300c9f379b6e9a
Jordan Grubb <imjordangrubb@gmail.com>
2020-12-16 17:36:35 -0500

    different sub-headline

--- mod.ts
+++ mod.ts
@@ -39,7 +39,7 @@ const helloVno: string =
   <p>
     For a guide and preview of our osLabs repo<br>
     check out 
-    <a href="https://github.com/oslabs-beta/vno" target="_blank" rel="noopener">&nbsp;vue-cli documentation</a>.
+    <a href="https://github.com/oslabs-beta/vno" target="_blank" rel="noopener">&nbsp;vno documentation</a>.
   </p>
   <h3>Installed CLI Plugins</h3>
   <ul>
